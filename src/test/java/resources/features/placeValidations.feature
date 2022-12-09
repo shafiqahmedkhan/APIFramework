e3fadde91a0ff75bfa21a4c8dc3945a9fb6666d2
Shafiq Ahmed Khan <shafiqahmedkhan@shafiqs-mbp.home>
2022-12-09 22:12:48 +0000

    wip

--- src/test/java/resources/features/placeValidations.feature
+++
@@ -1,7 +0,0 @@
-Feature: Validating Place API's
-  Scenario: Verify if Place is being successfully added AddPlaceAPI
-    Given Add Place Payload
-    When user calls "AddPlaceAPI" with Post http request
-    Then the API call is success with status code 200
-    And "status" in response body is "OK"
-    And "response" in response body is "APP"
--- src/test/java/stepDefinition/RunCukeTest.java
+++ src/test/java/stepDefinition/RunCukeTest.java
@@ -5,6 +5,6 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/resources/features/placeValidations.feature", glue = {"stepDefinition"})
+@CucumberOptions(features = "src/test/resources/features/placeValidations.feature", glue = {"src/test/java/stepDefinition"})
 public class RunCukeTest {
 }
