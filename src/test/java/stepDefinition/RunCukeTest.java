2df3fdd88ee3270242b56606e6d75aea04062f2e
Shafiq Ahmed Khan <shafiqahmedkhan@shafiqs-mbp.home>
2022-12-09 18:39:42 +0000

    first commit

---
+++ src/test/java/stepDefinition/RunCukeTest.java
@@ -0,0 +1,10 @@
+package stepDefinition;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/main/resources/features", glue = {"stepDefinition"})
+public class RunCukeTest {
+}
