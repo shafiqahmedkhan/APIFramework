d408b72fc601e1b64ae0962b39757914dd365b70
Shafiq Ahmed Khan <shafiqahmedkhan@shafiqs-mbp.home>
2022-12-09 22:00:02 +0000

    wip

--- src/test/java/stepDefinition/RunCukeTest.java
+++ src/test/java/stepDefinition/RunCukeTest.java
@@ -5,6 +5,6 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/main/resources/features", glue = {"stepDefinition"})
+@CucumberOptions(features = "src/test/java/resources/features/placeValidations.feature", glue = {"stepDefinition"})
 public class RunCukeTest {
 }
--- src/test/java/stepDefinition/stepDefinition.java
+++ src/test/java/stepDefinition/stepDefinition.java
@@ -4,7 +4,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class StepDefinitions {
+public class stepDefinition {
 
     @Given("Add Place Payload")
     public void add_place_payload() {
@@ -12,20 +12,15 @@ public class StepDefinitions {
     }
     @When("user calls {string} with Post http request")
     public void user_calls_with_post_http_request(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Hello World");
     }
     @Then("the API call is success with status code {int}")
     public void the_api_call_is_success_with_status_code(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Hello World");
     }
     @Then("{string} in response body is {string}")
     public void in_response_body_is(String string, String string2) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Hello World");
     }
-
-
 
 }
